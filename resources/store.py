a41c33ff29dbb9403fc9d0f89ca03dc2017e3f84
Bung Danar <danar.g.putera@gmail.com>
2021-04-08 09:41:50 +0700

    Added store schema

--- resources/store.py
+++ resources/store.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 
 from models.store import StoreModel
+from schemas.store import StoreSchema
 
 
 STORE_NOT_FOUND = "Store not found"
@@ -8,13 +9,16 @@ NAME_ALREADY_EXISTS = "A store with name '{}' already exists"
 ERROR_INSERTING = "An error occurred when creating the store"
 STORE_DELETED = "Store deleted"
 
+store_schema = StoreSchema()
+store_list_schema = StoreSchema(many=True)
+
 
 class Store(Resource):
     @classmethod
     def get(cls, name: str):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
+            return store_schema.dump(store)
 
         return {"message": STORE_NOT_FOUND}, 404
 
@@ -23,13 +27,13 @@ class Store(Resource):
         if StoreModel.find_by_name(name):
             return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
 
-        store = StoreModel(name)
+        store = StoreModel(name=name)
         try:
             store.save_to_db()
         except:
             return {"message": ERROR_INSERTING}, 500
 
-        return store.json(), 201
+        return store_schema.dump(store), 201
 
     @classmethod
     def delete(cls, name: str):
@@ -43,4 +47,4 @@ class Store(Resource):
 class StoreList(Resource):
     @classmethod
     def get(cls):
-        return {"stores": [store.json() for store in StoreModel.find_all()]}
+        return {"stores": store_list_schema.dump(StoreModel.find_all())}
